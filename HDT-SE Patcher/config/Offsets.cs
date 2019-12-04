68b4dcb71911887720a28e3b682e0adfd2b1680c
aers <aers00@gmail.com>
2019-12-03 20:23:10 -0800

    Add project files.

---
+++ HDT-SE Patcher/config/Offsets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace HDT_SE_Patcher.config
+{
+    class Offsets
+    {
+        [JsonPropertyName("versions")]
+        public List<string> AvailableVersions { get; set; }
+        [JsonPropertyName("files")]
+        public Dictionary<String, HDTFile> Files { get; set; }
+    }
+}
